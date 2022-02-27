0883203e6d376cd576bb743de4fcf8b3ad613366
Vanshul-Goyal-code <96768035+Vanshul-Goyal-code@users.noreply.github.com>
2022-02-27 22:38:12 +0530

    Create output.py

---
+++ output.py
@@ -0,0 +1,106 @@
+Python 3.10.1 (v3.10.1:2cd268a3a9, Dec  6 2021, 14:28:59) [Clang 13.0.0 (clang-1300.0.29.3)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=========== RESTART: /Users/vanshulgoyal/Python/Assignment4.py/Q1.py ===========
+
+Question 1
+
+Move disc 1 from source A to destination C
+Move disc 2 from source A to destination C
+Move disc 1 from source B to destination A
+Move disc 3 from source A to destination C
+Move disc 1 from source B to destination A
+Move disc 2 from source B to destination A
+Move disc 1 from source C to destination B
+
+=========== RESTART: /Users/vanshulgoyal/Python/Assignment4.py/Q2.py ===========
+
+Question 2
+
+Enter the number of rows for Pascal's Triangle: 5
+Using recursions to create pascal's triangle: 
+        1   
+      1   1   
+    1   2   1   
+  1   3   3   1   
+1   4   6   4   1   
+Using loops to create pascal's triangle:
+        1   
+      1   1   
+    1   2   1   
+  1   3   3   1   
+1   4   6   4   1   
+
+=========== RESTART: /Users/vanshulgoyal/Python/Assignment4.py/Q3.py ===========
+
+Question 3
+
+Enter the first number: 10
+Enter the first number: 7
+Questiont:1
+Remainder:3
+
+(a)
+True
+
+(b)
+All the values are non zero
+
+(c)
+After adding 4,5,6 :  [1, 3, 4, 5, 6]
+Values greater than 4 are :  [5, 6]
+
+(d)
+{1, 3, 4, 5, 6}
+
+(e)
+Immutable set :  frozenset({1, 3, 4, 5, 6})
+
+(f)
+-5818843836990437977
+
+=========== RESTART: /Users/vanshulgoyal/Python/Assignment4.py/Q4.py ===========
+
+Question 4
+
+Student created
+Student created
+Student created
+Deleting...
+Student deleted
+
+=========== RESTART: /Users/vanshulgoyal/Python/Assignment4.py/Q5.py ===========
+
+Question 5
+
+Record of all employees
+
+Employee Name :  Mehak
+Employee Salary :  40000
+
+
+Employee Name :  Ashok
+Employee Salary :  50000
+
+
+Employee Name :  Viren
+Employee Salary :  60000
+
+
+(a)
+Updated record of Mehak:
+Employee Name :  Mehak
+Employee Salary :  40000
+
+
+(b)
+Record of Vivek deleted
+
+=========== RESTART: /Users/vanshulgoyal/Python/Assignment4.py/Q6.py ===========
+
+Question 6
+
+Friend 1, please enter your word : arc
+Friend 2, please enter your word : car
+Is the new word meaningful?(Y/N) : y
+Your friendship is real
